4ce4b8397f7b75531d384211affecfba4164283b
mamjerez <correomam@gmail.com>
2019-03-10 07:28:20 +0100

    Añado cadena conexion para VPSmam

--- server/database.js
+++ server/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mean-crud';
+// const URI = 'mongodb://localhost/mean-crud';
+const URI = 'mongodb://139.59.152.136:27017/mean-crud';
 
 mongoose.connect(URI)
     .then(db => console.log('db is connected'))
